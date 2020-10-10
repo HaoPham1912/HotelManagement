0d5ddce798ebdec47ef2d397165a6c7f5e1fe3bd
16110035_Lê Văn Duy <47024529+duyle0207@users.noreply.github.com>
2020-10-10 12:09:33 +0700

    DuyLV7 update fe
    DuyLV7 update fe

--- h2fe/src/main.js
+++ h2fe/src/main.js
@@ -1,8 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
 
-Vue.config.productionTip = false
+window.Vue = require('vue');
+import Vue from 'vue'
+
+import App from './App.vue'
+import VueRouter from "vue-router";
+
+
+
+/* PrimeVue Control */
+
+Vue.use(VueRouter);
+
+
+import router from './route';
+import store from './store';
+
+
+
+// const router = routes;
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    store,
+    router,
+    render: h => h(App),
+}).$mount('#app');
