88b93efebb90fe4a7e1fe478362be7ca1c837f61
16110094-NguyenThiDieuHuyen <37243044+16110094-NguyenThiDieuHuyen@users.noreply.github.com>
2020-12-06 11:36:16 +0700

    create mapper

---
+++ HotelManagementSystem/src/main/java/com/h2/hotelmangement/util/ModelMapperUtil.java
@@ -0,0 +1,60 @@
+package com.h2.hotelmangement.util;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ModelMapperUtil {
+
+    private static ModelMapper modelMapper = new ModelMapper();
+
+    static {
+        modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+    }
+
+    /**
+     * Hide from public usage.
+     */
+    private ModelMapperUtil() {
+    }
+
+    /**
+     * @param <D>      type of result object.
+     * @param <T>      type of source object to map from.
+     * @param entity   entity that needs to be mapped.
+     * @param outClass class of result object.
+     * @return new object of outClass type.
+     */
+    public static <T, D> D map(final T entity, Class<D> outClass) {
+        return modelMapper.map(entity, outClass);
+    }
+
+    /**
+     * Note: outClass object must have default constructor with no arguments
+     *
+     * @param entityList list of entities that needs to be mapped
+     * @param outCLass   class of result list element
+     * @param <D>        type of objects in result list
+     * @param <T>        type of entity in entityList
+     * @return list of mapped object with <D> type.
+     */
+    public static <T, D> List<D> mapAll(final Collection<T> entityList, Class<D> outCLass) {
+        return entityList.stream()
+                .map(entity -> map(entity, outCLass))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     *
+     * @param source      object to map from
+     * @param destination object to map to
+     */
+    public static <S, D> D map(final S source, D destination) {
+        modelMapper.map(source, destination);
+        return destination;
+    }
+}
